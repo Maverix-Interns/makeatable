c11d1bfb079097efe5be3df271571e1f67f8b8ab
erfanhabeeb666 <erfanhabeeb30@gmail.com>
2023-10-02 11:57:13 +0530

    Done authentication
    //todo do authorization and various exception handling for all the apis

--- src/main/java/com/maverix/makeatable/config/Security/JwtService.java
+++ src/main/java/com/maverix/makeatable/config/Security/JwtService.java
@@ -1,5 +1,6 @@
 package com.maverix.makeatable.config.Security;
 
+import com.maverix.makeatable.services.UserService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -17,8 +18,17 @@ import java.util.function.Function;
 @Service
 public class JwtService {
     private static final String SECRET_KEY ="4e8e3a7215d4834a0bfade46cf9b9134104f2be3fea31faee6fbb54ad4fd4761";
+    private final UserService userService;
+
+    public JwtService(UserService userService) {
+        this.userService = userService;
+    }
+
     public String extractUsername(String token) {
         return extractClaim(token,Claims::getSubject);
+    }
+    public String extractId(String token) {
+        return extractClaim(token,Claims::getId);
     }
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
         final Claims claims=extractAllClaims(token);
@@ -31,14 +41,16 @@ public class JwtService {
             Map<String, Object> extraClaims,
             UserDetails userDetails
     ){
-        return  Jwts
+        return String.valueOf(Jwts
                 .builder()
                 .setClaims(extraClaims)
+                .setId(userService.getIdfromUsername(userDetails.getUsername()))
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis()+1000*60*24))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
-                .compact();
+                .compact()
+        );
     }
     public boolean isTokenValid(String token,UserDetails userDetails){
         final String username = extractUsername(token);
