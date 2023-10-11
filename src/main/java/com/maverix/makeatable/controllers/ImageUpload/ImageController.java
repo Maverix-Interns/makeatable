a1ecd8daf397a68fa4f2f905405d5a615343b009
erfanhabeeb666 <erfanhabeeb30@gmail.com>
2023-10-11 12:23:53 +0530

    Revert "image upload minor changes"
    This reverts commit 5d4eb216dcde6e2ec91fd5fbc759ba7ac7ce5715.

--- src/main/java/com/maverix/makeatable/controllers/ImageUpload/ImageController.java
+++ src/main/java/com/maverix/makeatable/controllers/ImageUpload/ImageController.java
@@ -6,7 +6,6 @@ import com.maverix.makeatable.models.User;
 import com.maverix.makeatable.services.RestaurantService;
 import com.maverix.makeatable.services.StorageService;
 import com.maverix.makeatable.services.UserService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,10 +17,9 @@ import java.util.Optional;
 @RequestMapping("/api/images")
 public class ImageController {
 
-    @Value("${app.baseURL}")
-    private String baseURL;
 
     private final UserService userService;
+
     private final StorageService storageService;
     private final RestaurantService restaurantService;
 
@@ -36,18 +34,14 @@ public class ImageController {
         Optional<Restaurant> optionalRestaurant = restaurantService.getRestaurantFullById(restaurantId);
 
         if (optionalRestaurant.isPresent()) {
-            Restaurant restaurant = optionalRestaurant.get();
-            String imageUrl = storageService.storeFile(file);
-            restaurant.setImageUrl(imageUrl);
+            Restaurant restaurant=optionalRestaurant.get();
+            restaurant.setImageUrl(storageService.storeFile(file));
             restaurantService.saveRestaurant(restaurant);
-
-            String resourceURL = baseURL + "uploads/" + file.getOriginalFilename();
-            return ResponseEntity.accepted().body("Image Uploaded. URL: " + resourceURL);
+            return ResponseEntity.accepted().body("Image Uploaded");
         } else {
             throw new ResourceNotFoundException("Restaurant not found with id: " + restaurantId);
         }
     }
-
     @PostMapping("/user/{userId}")
     public ResponseEntity<String> uploadImage(@PathVariable Long userId, @RequestParam("file") MultipartFile file) throws IOException {
         Optional<User> optionalUser = userService.getUserFullById(userId);
@@ -57,9 +51,7 @@ public class ImageController {
             String imageUrl = storageService.storeFile(file);
             user.setImageUrl(imageUrl);
             userService.saveUser(user);
-
-            String resourceURL = baseURL + "uploads/" + file.getOriginalFilename();
-            return ResponseEntity.accepted().body("Image Uploaded. URL: " + resourceURL);
+            return ResponseEntity.accepted().body("Image Uploaded");
         } else {
             throw new ResourceNotFoundException("User not found with id: " + userId);
         }
--- src/main/java/com/maverix/makeatable/services/StorageService.java
+++ src/main/java/com/maverix/makeatable/services/StorageService.java
@@ -1,13 +1,10 @@
 package com.maverix.makeatable.services;
 
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -15,25 +12,14 @@ import java.nio.file.Paths;
 @Service
 public class StorageService {
 
-    private static final String UPLOAD_DIR = "src/main/resources/static/uploads/";
+    private static final String UPLOAD_DIR = "./uploads/";
 
     public String storeFile(MultipartFile file) throws IOException {
-        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-        Path uploadPath = Paths.get(UPLOAD_DIR).toAbsolutePath().normalize();
-        Files.createDirectories(uploadPath);
-        Path filePath = uploadPath.resolve(fileName);
+        Path dir = Paths.get(UPLOAD_DIR);
+        Files.createDirectories(dir);
+        String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+        Path filePath = dir.resolve(fileName);
         Files.copy(file.getInputStream(), filePath);
         return filePath.toAbsolutePath().toString();
-    }
-
-    public Resource loadFileAsResource(String fileName) throws MalformedURLException {
-        Path filePath = Paths.get(UPLOAD_DIR).resolve(fileName).normalize();
-        Resource resource = new UrlResource(filePath.toUri());
-
-        if (resource.exists()) {
-            return resource;
-        } else {
-            throw new RuntimeException("File not found: " + fileName);
-        }
     }
 }
