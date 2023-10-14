f261f711fe6b7b9dbdf2b85ac07d3a08326c1164
erfanhabeeb666 <erfanhabeeb30@gmail.com>
2023-10-14 11:06:49 +0530

    Update WebConfig.java

--- src/main/java/com/maverix/makeatable/config/WebConfig.java
+++ src/main/java/com/maverix/makeatable/config/WebConfig.java
@@ -1,11 +1,18 @@
 package com.maverix.makeatable.config;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Use your specific frontend origin(s)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
